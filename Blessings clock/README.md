dd0088cff6d7d7d6974100eb987ac48f6253ae8d
Kabir Sala <spittiepie@gmail.com>
2014-02-19 04:30:48 +0100

    Add blessing clock

---
+++ Blessings clock/README.md
@@ -0,0 +1,8 @@
+Blessing clock
+==============
+
+Simple ncurses clock (using blessing)
+
+Require:
+- [blessing](https://pypi.python.org/pypi/blessings/)
+- [arrow](https://pypi.python.org/pypi/arrow/0.4.2)
---
+++ Blessings clock/clock.py
@@ -0,0 +1,106 @@
+from blessings import Terminal
+import arrow
+import time
+import sys
+
+
+def print_array(array, x, y):
+    for l in array:
+        for c in l:
+            with t.location(x, y):
+                sys.stdout.write(c)
+        y += 1
+        print
+
+t = Terminal()
+
+uno = [['  #  '],
+       [' ##  '],
+       ['# #  '],
+       ['  #  '],
+       [' ### ']]
+
+due = [['#### '],
+       ['    #'],
+       [' ### '],
+       ['#    '],
+       [' ####']]
+
+tre = [['#####'],
+       ['    #'],
+       [' ### '],
+       ['    #'],
+       ['#####']]
+
+qua = [['#    '],
+       ['#    '],
+       ['# #  '],
+       ['#####'],
+       ['  #  ']]
+
+cin = [['#####'],
+       ['#    '],
+       ['#### '],
+       ['    #'],
+       ['#### ']]
+
+sei = [['#####'],
+       ['#    '],
+       ['#####'],
+       ['#   #'],
+       ['#####']]
+
+set = [['#####'],
+       ['    #'],
+       ['   # '],
+       ['  #  '],
+       ['  #  ']]
+
+ott = [['#####'],
+       ['#   #'],
+       ['#####'],
+       ['#   #'],
+       ['#####']]
+
+nov = [['#####'],
+       ['#   #'],
+       ['#####'],
+       ['    #'],
+       ['#####']]
+
+sep = [['     '],
+       ['  #  '],
+       ['     '],
+       ['  #  '],
+       ['     ']]
+
+
+while 1:
+    with t.fullscreen():
+        print t.clear()
+        y = 0
+        x = 0
+        for i in arrow.now().format('HH:mm:ss'):
+            if i == str(1):
+                print_array(uno, x, y)
+            if i == str(2):
+                print_array(due, x, y)
+            if i == str(3):
+                print_array(tre, x, y)
+            if i == str(4):
+                print_array(qua, x, y)
+            if i == str(5):
+                print_array(cin, x, y)
+            if i == str(6):
+                print_array(sei, x, y)
+            if i == str(7):
+                print_array(set, x, y)
+            if i == str(8):
+                print_array(ott, x, y)
+            if i == str(9):
+                print_array(nov, x, y)
+            if i == ':':
+                print_array(sep, x, y)
+            x += 6
+        time.sleep(1)
+
